87fd6ffc51c1ff24a4a507d9e501bd35ae7284e1
Alain <alain.wackenheim@protonmail.com>
2019-10-28 11:12:15 +0100

    Ajout SampleSpringBootApplication

---
+++ src/main/java/org/cnam/sample/SampleSpringBootApplication.java
@@ -0,0 +1,21 @@
+package org.cnam.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@ComponentScan(basePackages = { "org.cnam.*" })
+@EntityScan("org.cnam.sample.model")
+@EnableJpaRepositories("org.cnam.sample.repository")
+@EnableTransactionManagement
+@EnableAutoConfiguration
+public class SampleSpringBootApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleSpringBootApplication.class, args);
+    }
+
+}
